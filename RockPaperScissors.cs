07ded17debcab975401a9a208548daf2685f0123
Yurii Siechin <siechinyurii@gmail.com>
2022-06-03 02:00:20 +0300

    Added RockPaperScissors.splitIntoTeams()

--- RockPaperScissors.cs
+++ RockPaperScissors.cs
@@ -7,6 +7,8 @@ namespace Rock_Paper_Scissors
 {
     class RockPaperScissors
     {
+        int roundN;
+
         Dictionary<string, string> hierarchy = new Dictionary<string, string>()
         {
             {"Rock", "Scissors"},
@@ -15,18 +17,59 @@ namespace Rock_Paper_Scissors
         };
 
         List<string> players;
+        List<string> remaining;
+        List<List<string>> teams;
 
         public RockPaperScissors(List<string> names)
         {
             players = names;
+            roundN = 0;
         }
 
         public void start()
         {
+            // what is better: recursion or while ???
 
+            remaining = players;
+
+            do
+            {
+                roundN++;
+                splitIntoTeams();
+                round();
+            } while (remaining.Count > 0);
+            
+        }
+
+        private void splitIntoTeams()
+        {
+            int rand;
+            List<string> team;
+
+            for (int i = 2; i < remaining.Count; i += 3)
+            {
+                team = new List<string>();
+
+                for (; i < 3; i++)
+                {
+                    rand = new Random().Next(remaining.Count);
+                    team.Add(remaining[rand]);
+                    remaining.RemoveAt(rand);
+                }
+            }
+
+            foreach (string s in remaining)
+            {
+                teams[new Random().Next(teams.Count)].Add(s);
+            }
+        }
+
+        private void round()
+        {
+            
         }
         
-
+        // TODO: make class Player
 
 
     }
